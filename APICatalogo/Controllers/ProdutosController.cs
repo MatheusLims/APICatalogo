472295a4475c12cf68d9bec2bfb6326ba4c69575
Matheus Lima <devbymatheus@gmail.com>
2026-01-06 22:25:24 -0300

    Add project files.

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,84 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get() 
+        {
+            var produtos = _context.Produtos.ToList();
+            //var produtos = _context.Produtos.Take(10).ToList(); -> Retorna um valor máximo da requisição sem necessitar retornar TODOS os produtos
+            if (produtos is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name ="ObterProduto")]
+
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound($"Produto com id={id} não encontrado.");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest("Dados inválidos");
+
+            _context.Produtos.Add(produto); // adicionar produto ao contexto
+            _context.SaveChanges(); //persistir no banco de dados
+
+            return new CreatedAtRouteResult("ObterProduto", // Adiciona o Status 201 - Created
+                new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound($"Produto com id={id} não encontrado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
